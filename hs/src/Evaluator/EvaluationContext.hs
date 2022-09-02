f6e2d6ecd99cbb819e8fb2faa7a9aa71807defe4
fg1989 <fgazzett@gmail.com>
2022-09-03 00:07:24 +0200

    Amélioration de l'évaluateur

--- hs/src/Evaluator/EvaluationContext.hs
+++ hs/src/Evaluator/EvaluationContext.hs
@@ -1,41 +1,51 @@
 module Evaluator.EvaluationContext where
 
+import Data.List.NonEmpty
 import Model.Model
 
 newtype Error = Error String deriving (Show)
 
 data GlobalContext = GlobalContext {fonctions :: [Fonction], source :: Int}
 
+data PreFunctionContext = PreFunctionContext {param :: [Int], globalContext :: GlobalContext}
+
 -- structure de donneée plus efficace que la liste pour l'accès indexé (arbre ?) ?
-data FunctionContext = EvaluationContext
-  { index :: ExpressionIndex,
-    stack :: [Int],
-    param :: [Int],
-    globalContext :: GlobalContext
-  }
+data FunctionContext = FunctionContext {index :: ExpressionIndex, stack :: NonEmpty Int, initContext :: PreFunctionContext}
 
-initEvaluationContext :: [Int] -> GlobalContext -> Either Error FunctionContext
-initEvaluationContext funcParam context =
-  Right $ EvaluationContext (-1) [] funcParam context
+initEvaluationContext :: Int -> PreFunctionContext -> FunctionContext
+initEvaluationContext val = FunctionContext 0 (val :| [])
 
+-- Existe il une meilleure manière de faire évoluer le contexte (éviter le toList) ?
 evolveContext :: FunctionContext -> Int -> FunctionContext
-evolveContext context val =
-  EvaluationContext (index context + 1) (val : stack context) (param context) (globalContext context)
+evolveContext context@(FunctionContext currentIndex currentStack _) val =
+  context {index = currentIndex + 1, stack = val :| toList currentStack}
 
 readContext :: FunctionContext -> ExpressionIndex -> Either Error Int
 readContext context pos
-  | index context >= pos = Right $ stack context !! exprIndex (index context - pos)
-  | otherwise = Left $ Error "Invalid index expression"
+  | index context < pos = Left $ Error "Invalid index expression"
+  | otherwise = Right $ stack context Data.List.NonEmpty.!! exprIndex (index context - pos)
 
-readFuncInContext :: FunctionContext -> FonctionIndex -> Either Error Fonction
+readFuncInContext :: GlobalContext -> FonctionIndex -> Either Error Fonction
 readFuncInContext context (FonctionIndex index) =
-  safeRead (fonctions $ globalContext context) index $ Error "Invalid func index"
+  safeRead (fonctions context) index $ Error "Invalid func index"
 
-readParamInContext :: FunctionContext -> ParamIndex -> Either Error Int
+-- Peut on éviter la duplication
+readFuncInContext' :: PreFunctionContext -> FonctionIndex -> Either Error Fonction
+readFuncInContext' = readFuncInContext . globalContext
+
+-- Peut on éviter la duplication
+readFuncInContext'' :: FunctionContext -> FonctionIndex -> Either Error Fonction
+readFuncInContext'' = readFuncInContext' . initContext
+
+readParamInContext :: PreFunctionContext -> ParamIndex -> Either Error Int
 readParamInContext context (ParamIndex index) =
   safeRead (param context) index $ Error "Invalid param index"
 
+-- Peut on éviter la duplication
+readParamInContext' :: FunctionContext -> ParamIndex -> Either Error Int
+readParamInContext' = readParamInContext . initContext
+
 safeRead :: [a] -> Int -> Error -> Either Error a
 safeRead list pos errorMessage
-  | length list > pos = Right $ list !! pos
-  | otherwise = Left errorMessage
+  | Prelude.length list <= pos = Left errorMessage
+  | otherwise = Right $ list Prelude.!! pos
--- hs/src/Evaluator/Evaluator.hs
+++ hs/src/Evaluator/Evaluator.hs
@@ -4,6 +4,7 @@ module Evaluator.Evaluator
   )
 where
 
+import Data.List.NonEmpty
 import Evaluator.EvaluationContext
 import Model.Model
 
@@ -14,29 +15,26 @@ runParam :: Application -> [Int] -> Either Error Int
 runParam (Application mainFunc otherFunc) =
   runFonction mainFunc (GlobalContext otherFunc 0)
 
--- est ce que c'est optimisé de continuer a parcourir la liste si on s'arrete
 runFonction :: Fonction -> GlobalContext -> [Int] -> Either Error Int
-runFonction (Fonction exprs paramCount) context funcParam
-  | length funcParam /= paramCount = Left $ Error "Invalid number of call arguments"
+runFonction (Fonction (first :| others) paramCount) context funcParam
+  | Prelude.length funcParam /= paramCount = Left $ Error "Invalid number of call arguments"
   | otherwise =
-    head . stack
-      <$> foldl
-        evalExpressionInContext
-        (initEvaluationContext funcParam context)
-        exprs
+    let preFuncContext = PreFunctionContext funcParam context
+     in let firstValue = evalExpressionWithPreContext first preFuncContext
+         in do
+              realFirstValue <- firstValue
+              evalExpressions others (initEvaluationContext realFirstValue preFuncContext)
 
-evalExpressionInContext :: Either Error FunctionContext -> Expression -> Either Error FunctionContext
+evalExpressions :: [Expression] -> FunctionContext -> Either Error Int
+evalExpressions (current : nexts) context =
+  evalExpressionInContext context current >>= evalExpressions nexts
+evalExpressions [] (FunctionContext _ (final :| other) _) = Right final
+
+evalExpressionInContext :: FunctionContext -> Expression -> Either Error FunctionContext
 evalExpressionInContext context expr =
-  do
-    contextValue <- context
-    expressionValue <- evalExpression expr contextValue
-    return $ evolveContext contextValue expressionValue
+  evolveContext context <$> evalExpression expr context
 
 evalExpression :: Expression -> FunctionContext -> Either Error Int
-evalExpression InvalidExpression _ = Left $ Error "Invalid Expression"
---
-evalExpression (ConstExpression val) _ = Right val
---
 evalExpression (AdditionExpression first second) context =
   do
     param1 <- readContext context first
@@ -47,14 +45,30 @@ evalExpression (ConditionalExpression cond notNullExpression nullExpression) con
   let expressionSelector x = if x == 0 then nullExpression else notNullExpression
    in readContext context cond >>= \x -> evalExpression (expressionSelector x) context
 --
-evalExpression (ParamExpression paramIndex) context =
-  readParamInContext context paramIndex
---
 evalExpression (FuncCall funcIndex params) context =
   do
-    func <- readFuncInContext context funcIndex
+    func <- readFuncInContext'' context funcIndex
     paramValue <- mapM (readContext context) params
-    runFonction func (globalContext context) paramValue
+    runFonction func (globalContext $ initContext context) paramValue
 --
-evalExpression (ExternalExpression _ _) _ =
-  undefined
+evalExpression expr context =
+  evalExpressionWithPreContext expr (initContext context)
+
+evalExpressionWithPreContext :: Expression -> PreFunctionContext -> Either Error Int
+evalExpressionWithPreContext InvalidExpression _ = Left $ Error "Invalid Expression"
+--
+evalExpressionWithPreContext (ConstExpression val) _ = Right val
+--
+evalExpressionWithPreContext (ParamExpression paramIndex) context =
+  readParamInContext context paramIndex
+--
+evalExpressionWithPreContext (FuncCall funcIndex []) context =
+  do
+    func <- readFuncInContext' context funcIndex
+    runFonction func (globalContext context) []
+--
+evalExpressionWithPreContext (ExternalExpression _ _) _ =
+  undefined -- TODO
+  --
+evalExpressionWithPreContext _ _ =
+  Left $ Error "Invalid index expression"
